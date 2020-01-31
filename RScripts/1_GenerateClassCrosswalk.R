035f287c9e7d96fc76f3eba429543f3039af6105
chloedebyser <chloe.debyser@mail.mcgill.ca>
2020-01-31 17:52:48 -0500

    Separated scripts for generating crosswalk and Reference Condition Table

---
+++ RScripts/1_GenerateClassCrosswalk.R
@@ -0,0 +1,83 @@
+#### a202 - TNC LANDFIRE Support
+#### Script by Chloé Debyser
+
+#### 1. Generate Class Crosswalk 
+
+###########################################################################################################################
+# This code:                                                                                                              #
+# - Reads Word documents containing class definitions for each model                                                      #
+# - Creates a crosswalk for stage Classes A-E per Model | Cover Type | Structural Stage                                   #
+###########################################################################################################################
+
+#### Workspace ####
+# Packages
+library(tidyverse)
+library(magrittr)
+library(textreadr)
+
+# Directories
+docDir <- "E:/Data/Model Description Docs/BpSDocs_Parsed/"
+resultsDir <- "E:/Results/"
+
+#### Create Class Crosswalk ####
+# Define functions extracting desired attributes from text lines
+      # Attribute: Class
+get.class <- function(x){
+  substr(x, start=7, stop=7)
+}
+
+      # Attribute: Cover Type
+get.coverType <- function(x){
+  start <- str_locate_all(x, "[A-Z]")[[1]][3,1]
+  stop <- str_locate_all(x, " - ")[[1]][1,1]-1
+  y <- substr(x, start=start, stop=stop)
+}
+
+      # Attribute: Structural Stage
+get.structuralStage <- function(x){
+  start <- str_locate_all(x, " - ")[[1]][1,1]+3
+  stop <- nchar(x)
+  y <- substr(x, start=start, stop=stop)
+}
+
+# Get all model names
+models <- list.files(docDir, pattern=".docx") %>%
+  substr(., start=1, stop=nchar(.)-5)
+
+# For each model, extract crosswalk from corresponding Word doc
+for(i in 1:length(models)){
+  # Extract text lines from Word doc
+  desc <- read_docx(paste0(docDir, models[i], ".docx")) %>%
+    .[which((substr(., start=1, stop=6) == "Class ") & (grepl(" - ", ., fixed=T)) & (!substr(., start=8, stop=8) == " "))]
+  
+  # Compile model crosswalk
+  modelCrosswalk <- data.frame(Model_Code = models[i],
+                               Class = sapply(desc, get.class),
+                               CoverType = sapply(desc, get.coverType),
+                               StructuralStage = sapply(desc, get.structuralStage),
+                               stringsAsFactors = F)
+  
+  # Add to master crosswalk
+  if(i == 1){
+    crosswalk <- modelCrosswalk
+  }else{
+    crosswalk %<>% bind_rows(., modelCrosswalk)
+  }
+}
+rm(modelCrosswalk, desc, i)
+
+# Cleanup non-standard document names
+crosswalk$Model_Code <- gsub(" ", "", crosswalk$Model_Code)
+crosswalk$Model_Code <- gsub(".doc", "", crosswalk$Model_Code)
+crosswalk$Model_Code <- sapply(crosswalk$Model_Code, function(x) ifelse(substr(x, start=nchar(x), stop=nchar(x)) == "_", substr(x, start=1, stop=nchar(x)-1), x))
+crosswalk$Model_Code <- gsub("_RS", "", crosswalk$Model_Code)
+
+# Change cover type/structural stage labels to codes
+      # Cover type
+crosswalk %<>% mutate(CoverType = sub(" Development ", "", CoverType))
+
+      # Structural stage
+crosswalk$StructuralStage <- sapply(crosswalk$StructuralStage, function(x){ifelse(x %in% c("All Structures", "All"), "ALL", ifelse(x == "Closed", "CLS", "OPN"))})
+
+# Save
+write.csv(crosswalk, paste0(resultsDir, "ClassCrosswalk.csv"), row.names = F)
--- RScripts/2_GenerateReferenceConditionTable.R
+++ RScripts/2_GenerateReferenceConditionTable.R
@@ -1,11 +1,10 @@
 #### a202 - TNC LANDFIRE Support
 #### Script by Chloé Debyser
 
-#### 1. Generate Reference Condition Table 
+#### 2. Generate Reference Condition Table 
 
 ###########################################################################################################################
 # This code:                                                                                                              #
-# - Creates a crosswalk for stage Classes A-E per Model | Cover Type | Structural Stage                                   #
 # - 
 ###########################################################################################################################
 
@@ -14,16 +13,19 @@
 library(rsyncrosim)
 library(tidyverse)
 library(magrittr)
-library(textreadr)
+library(openxlsx)
 
 # Directories
-docDir <- "E:/Data/Model Description Docs/BpSDocs_Parsed/"
 resultsDir <- "E:/Results/"
 
 # Input Parameters
 scenarioId <- 8808 # Id number of the scenario of interest
 timeStart <- 501 # First time step of interest for analyses
 timeStop <- 1000 # Last time step of interest for analyses
+
+# Tabular data
+crosswalk <- read.csv(paste0(resultsDir, "ClassCrosswalk.csv"))
+FRG_rules <- read.xlsx("E:/Data/Classification Rules - FRG/Computing AllFireFRI and % Fires.xlsx")
 
 # ST-Sim outputs
       # Library
@@ -44,69 +46,6 @@ transitions <- datasheet(scenario, "OutputStratumTransition") %>% # Load
 
 names <- datasheet(scenario, "Stratum") # Load
 
-#### Create Class Crosswalk ####
-# Define functions extracting desired attributes from text lines
-      # Attribute: Class
-get.class <- function(x){
-  substr(x, start=7, stop=7)
-}
-
-      # Attribute: Cover Type
-get.coverType <- function(x){
-  start <- str_locate_all(x, "[A-Z]")[[1]][3,1]
-  stop <- str_locate_all(x, " - ")[[1]][1,1]-1
-  y <- substr(x, start=start, stop=stop)
-}
-
-      # Attribute: Structural Stage
-get.structuralStage <- function(x){
-  start <- str_locate_all(x, " - ")[[1]][1,1]+3
-  stop <- nchar(x)
-  y <- substr(x, start=start, stop=stop)
-}
-
-# Get all model names
-models <- list.files(docDir, pattern=".docx") %>%
-  substr(., start=1, stop=nchar(.)-5)
-
-# For each model, extract crosswalk from corresponding Word doc
-for(i in 1:length(models)){
-  # Extract text lines from Word doc
-  desc <- read_docx(paste0(docDir, models[i], ".docx")) %>%
-    .[which((substr(., start=1, stop=6) == "Class ") & (grepl(" - ", ., fixed=T)) & (!substr(., start=8, stop=8) == " "))]
-  
-  # Compile model crosswalk
-  modelCrosswalk <- data.frame(Model_Code = models[i],
-                               Class = sapply(desc, get.class),
-                               CoverType = sapply(desc, get.coverType),
-                               StructuralStage = sapply(desc, get.structuralStage),
-                               stringsAsFactors = F)
-  
-  # Add to master crosswalk
-  if(i == 1){
-    crosswalk <- modelCrosswalk
-  }else{
-    crosswalk %<>% bind_rows(., modelCrosswalk)
-  }
-}
-rm(modelCrosswalk, desc, i)
-
-# Cleanup non-standard document names
-crosswalk$Model_Code <- gsub(" ", "", crosswalk$Model_Code)
-crosswalk$Model_Code <- gsub(".doc", "", crosswalk$Model_Code)
-crosswalk$Model_Code <- sapply(crosswalk$Model_Code, function(x) ifelse(substr(x, start=nchar(x), stop=nchar(x)) == "_", substr(x, start=1, stop=nchar(x)-1), x))
-crosswalk$Model_Code <- gsub("_RS", "", crosswalk$Model_Code)
-
-# Change cover type/structural stage labels to codes
-      # Cover type
-crosswalk %<>% mutate(CoverType = sub(" Development ", "", CoverType))
-
-      # Structural stage
-crosswalk$StructuralStage <- sapply(crosswalk$StructuralStage, function(x){ifelse(x %in% c("All Structures", "All"), "ALL", ifelse(x == "Closed", "CLS", "OPN"))})
-
-# Save
-write.csv(crosswalk, paste0(resultsDir, "ClassCrosswalk.csv"), row.names = F)
-
 #### Initiate Reference Condition Table ####
 # Start with Model_Code
 table <- data.frame(Model_Code = unique(crosswalk$Model_Code), stringsAsFactors = F)
@@ -123,12 +62,12 @@ if((sum(table$BpS_Name == "character(0)") > 0) | (sum(is.na(table$BpS_Name)) > 0
 
 #### Indicator 1: % of the landscape in each class ####
 # Add Class label to states dataframe
-      # Create a Model_StateClassID identifier
+# Create a Model_StateClassID identifier
 crosswalk %<>% mutate(StateClassID = paste(CoverType, StructuralStage, sep=":"),
                       Model_StateClassID = paste(Model_Code, StateClassID, sep=":"))
 states %<>% mutate(Model_StateClassId = paste(Model_Code, StateClassID, sep=":"))
 
-      # Extract Class
+# Extract Class
 states$Class <- sapply(states$Model_StateClassId, function(x) crosswalk$Class[crosswalk$Model_StateClassID == x])
 states %<>% select(c(Model_Code, Iteration, Timestep, Class, Amount, AgeMin, AgeMax)) %>%
   mutate(Class = as.character(Class)) %>%
